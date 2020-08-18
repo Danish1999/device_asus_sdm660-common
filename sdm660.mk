ad0bee8946888861f3e3c9be31907827b8b3323e
Danish1999 <danish01031999@gmail.com>
2020-08-18 22:29:17 +0530

    sdm660-common: Build btconfigstore from source

--- sdm660.mk
+++ sdm660.mk
@@ -104,7 +104,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default
+    audio.bluetooth.default \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libsndmonitor \
